c471cb59925472e714d72c29eae46f38f6519578
Margaret <wanjirundirangu43@gmail.com>
2022-06-08 13:29:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<=400) {
+    return "This one is on me!";
+  }
+  else if (someValue>2500){
+    return "No can do.";
+}
+  else if (someValue>2000){
+    return "I will gladly take your thirty bucks.";
+  }
+} 
+
+function ternaryCheckCity(city){
+  return city ==='NYC'?"Ok, sounds good.":`No go.`
+}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+        return "Thank you so much."
+      case 'not as generous':
+       return "Thank you."
+      default:
+        return "Bye."
+
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
